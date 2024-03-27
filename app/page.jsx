2a6c16c22a3539b15d092fe7975049a38a4a7028
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2024-03-27 16:27:56 +0530

    First Update

---
+++ app/page.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id='container'>
+<div id="nav_bar">
+
+<div id='Logo_nav'>
+  <h2 id='Logo_txt'> F I R E</h2>
+</div>
+
+     <nav>
+      <ul id='list'>
+        <li className='item'>Home</li>
+        <li className='item'>Posts</li>
+        <li className='item'>Pages</li>
+      </ul>
+     </nav>
+ <div id="icon">
+ <i class="ri-menu-5-line"></i>
+ </div>
+</div>
+<div id="login">
+    <h1>L O G I  N</h1>
+    <input className='Username' type="text"  placeholder='ENTER  USERNAME' /> <br />
+    <input className='Username' type="password"placeholder='ENTER YOUR PASSWORD' />
+    <br />
+      <button id='login_btn'>L O G I N</button>
+     </div>
+      <h2 id='devinfo'> &copy; FAIROZ AHMAD SHEIKH</h2>
+    </div>
+  )
+}
+
+export default Main
